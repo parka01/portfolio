81e08d59d4136c2b48c690758df062c15f1bd013
parka01 <68191916+parka01@users.noreply.github.com>
2022-02-24 04:23:55 +0900

    Keydown

--- javascript/Drum Kit/index.js
+++ javascript/Drum Kit/index.js
@@ -46,3 +46,38 @@ function drumClick(){
 
   }
 }
+document.addEventListener("keydown", drumKeyboard);
+function drumKeyboard(event){
+  var pressedKey = event.key;
+  switch (pressedKey) {
+    case "w":
+      const drumSound1 = new Audio("sounds/tom-1.mp3");
+            drumSound1.play();
+    break;
+    case "a":
+      const drumSound2 = new Audio("sounds/tom-2.mp3");
+            drumSound2.play();
+    break;
+    case "s":
+      const drumSound3 = new Audio("sounds/tom-3.mp3");
+            drumSound3.play();
+    break;
+    case "d":
+      const drumSound4 = new Audio("sounds/tom-4.mp3");
+            drumSound4.play();
+    break;
+    case "j":
+      const drumSound5 = new Audio("sounds/crash.mp3");
+            drumSound5.play();
+    break;
+    case "k":
+      const drumSound6 = new Audio("sounds/kick-bass.mp3");
+            drumSound6.play();
+    break;
+    case "l":
+      const drumSound7 = new Audio("sounds/snare.mp3");
+            drumSound7.play();
+    break;
+    default: console.log(event.code);
+  }
+}
