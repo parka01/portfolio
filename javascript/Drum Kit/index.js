59519a6dfbb680d577a4f34226a654f06e5792be
parka01 <68191916+parka01@users.noreply.github.com>
2022-02-24 03:42:13 +0900

    Drum kit

---
+++ javascript/Drum Kit/index.js
@@ -0,0 +1,48 @@
+var buttonLength = document.querySelectorAll(".drum").length;
+
+for(var i=0; i<buttonLength; i++){
+  document.querySelectorAll("button")[i].addEventListener("click", drumClick
+  );
+}
+
+
+function drumClick(){
+  //alert("Clicked!");
+
+  var assignedCharacter = this.className;
+  switch (assignedCharacter) {
+    case "w drum":
+            const drumSound1 = new Audio("sounds/tom-1.mp3");
+            drumSound1.play();
+      break;
+
+    case "a drum":
+            const drumSound2 = new Audio("sounds/tom-2.mp3");
+            drumSound2.play();
+    break;
+    case "s drum":
+            const drumSound3 = new Audio("sounds/tom-3.mp3");
+            drumSound3.play();
+      break;
+    case "d drum":
+            const drumSound4 = new Audio("sounds/tom-4.mp3");
+            drumSound4.play();
+      break;
+    case "j drum":
+            const drumSound5 = new Audio("sounds/crash.mp3");
+            drumSound5.play();
+      break;
+    case "k drum":
+            const drumSound6 = new Audio("sounds/kick-bass.mp3");
+            drumSound6.play();
+      break;
+    case "l drum":
+            const drumSound7 = new Audio("sounds/snare.mp3");
+            drumSound7.play();
+      break;
+    default: console.log(assignedCharacter);
+
+
+
+  }
+}
